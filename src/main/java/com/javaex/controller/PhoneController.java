2442e79950e81f56cfb608ed2edfebe0fc825f5d
jongbin-choi <kooma1123@naver.com>
2022-06-13 16:14:20 +0900

    phonebook3

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -1,6 +1,11 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,25 +23,121 @@ public class PhoneController{
 	//메소드 - gs
 	
 	//메소드 - 일반
+	//전화번호 리스트
+	@RequestMapping(value="/list", method= {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("PhoneController > list()");
+		
+		//Dao만들기
+		PhoneDao phoneDao = new PhoneDao();
+		List<PersonVo> personList = phoneDao.getPersonList();
+		
+		//ds 데이터보내기 ==> request attribute에 넣는다.
+		model.addAttribute("personList",personList);
+		
+		
+		return "/WEB-INF/views/list.jsp";
+	}
+	
+	//전화번호 업데이트
+	@RequestMapping(value="/update", method= {RequestMethod.GET, RequestMethod.POST})
+	public String update(@ModelAttribute PersonVo personVo) {
+		System.out.println("PhoneController > update()");
+		
+		//파라미터 꺼내기 + Vo로 묶어서 Ds로 메소드의 파라미터로 보내준다.
+		
+		//Dao만들기
+		PhoneDao phoneDao = new PhoneDao();
+		phoneDao.personUpdate(personVo);
+		
+		return "redirect:/list";
+	}
+	
+	
+	//전화번호 업데이트폼
+	@RequestMapping(value="/updateForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String updateForm(Model model ,@RequestParam("no") int no) {
+		System.out.println("PhoneController > updateForm()");
+
+		//Dao만들기
+		PhoneDao phoneDao = new PhoneDao();
+		PersonVo personVo = phoneDao.getPerson(no);
+		
+		model.addAttribute("personVo", personVo);
+		
+		return "/WEB-INF/views/updateForm.jsp";
+	}
+	
+	
+	//전화번호 삭제 (url에 PathVariable로 사용하는 방법)
+	@RequestMapping(value="/delete/{no}", method= {RequestMethod.GET, RequestMethod.POST})
+	public String delete2(@PathVariable("no") int no) {
+		System.out.println("PhoneController > delete()");
+		
+		//Dao만들기
+		PhoneDao phoneDao = new PhoneDao();
+		phoneDao.personDelete(no);
+		
+		System.out.println(no);
+		
+		return "redirect:/list";
+	}
+	
+	//전화번호 삭제 (url에 쿼리스트링으로 사용하는 방법)
+		@RequestMapping(value="/delete", method= {RequestMethod.GET, RequestMethod.POST})
+		public String delete(@RequestParam("no") int no) {
+			System.out.println("PhoneController > delete()");
+			
+			//Dao만들기
+			PhoneDao phoneDao = new PhoneDao();
+			phoneDao.personDelete(no);
+			
+			
+			return "redirect:/list";
+		}
+
 	//전화번호 등록
-	@RequestMapping(value="/write", method= {RequestMethod.GET, RequestMethod.POST})
-	public String write(@RequestParam("name") String name, 
-			@RequestParam("hp") String hp,
+		@RequestMapping(value="/write", method= {RequestMethod.GET, RequestMethod.POST})
+		public String write(@ModelAttribute PersonVo personVo) {
+			System.out.println("PhoneController > write()");
+			
+			//파라미터 꺼내기 + Vo로 묶어서 Ds로 메소드의 파라미터로 보내준다.
+			
+			//Dao로 저장하기
+			PhoneDao phoneDao = new PhoneDao();
+			int count = phoneDao.personInsert(personVo);
+			System.out.println(count);
+			
+			//리다이렉트 
+			//리스트로 리다이텍트 시킬 예정
+			
+			return "redirect:/list";
+		}
+		
+	
+	//전화번호 등록
+	@RequestMapping(value = "/write2", method = { RequestMethod.GET, RequestMethod.POST })
+	public String write2(@RequestParam("name") String name, @RequestParam("hp") String hp,
 			@RequestParam("company") String company) {
 		System.out.println("PhoneController > write()");
-		System.out.println(name);
-		System.out.println(hp);
-		System.out.println(company);
-		
+		/*
+		 System.out.println(name); 
+		 System.out.println(hp);
+		 System.out.println(company);
+		 */
+		//Vo만들기
 		PersonVo personVo = new PersonVo(name, hp, company);
 		System.out.println(personVo);
-		
+
 		//Dao로 저장하기
 		PhoneDao phoneDao = new PhoneDao();
 		int count = phoneDao.personInsert(personVo);
 		System.out.println(count);
-		
-		return "/WEB-INF/views/writeForm.jsp";
+
+		//리다이렉트
+		//리스트로 리다이텍트 시킬 예정
+
+		return "redirect:/list";
 	}
 	
 	
@@ -47,10 +148,6 @@ public class PhoneController{
 		
 		return "/WEB-INF/views/writeForm.jsp";
 	}
-	
-	
-	
-	
 	
 	
 	//테스트
